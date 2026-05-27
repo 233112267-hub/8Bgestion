5cf90c2b155791cbfcdea49b2f3db71e60c917ed
233112267-hub <233112267@upmh.edu.mx>
2026-05-27 00:06:10 -0600

    Configuracion de login principal

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from .models import Asesor, Asesorado
 
-# Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+
+    mensaje = ""
+
+    if request.method == "POST":
+
+        usuario = request.POST.get("usuario")
+        password = request.POST.get("password")
+
+        asesor = Asesor.objects.filter(
+            nombre_usuario=usuario,
+            contrasenia=password
+        ).first()
+
+        asesorado = Asesorado.objects.filter(
+            nombre_usuario=usuario,
+            contrasenia=password
+        ).first()
+
+        if asesor or asesorado:
+            mensaje = "Login correcto"
+        else:
+            mensaje = "Usuario o contraseña incorrectos"
+
+    return render(request, 'index.html', {
+        'mensaje': mensaje
+    })
